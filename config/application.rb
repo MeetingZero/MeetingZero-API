2390d33b726086ccbd00543417039d16e719508e
Arun Sood <arsood@gmail.com>
2020-06-08 22:19:18 -0700

    Add hosts to application config

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module MeetingzeroApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.hosts << 'api-dev.meetingzero.net'
+    config.hosts << 'api.meetingzero.net'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
