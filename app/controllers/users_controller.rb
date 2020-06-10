c86d45e29aac7eaeb0c82154a7440cbbaa0a1407
Arun Sood <arsood@gmail.com>
2020-06-09 20:17:37 -0700

    Handle case for already activated account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
   def activate_account
     user = User
-    .where(email: params[:email], account_activation_token: params[:token])
+    .where(email: params[:email])
     .first
 
     if !user
-      return render :text => "No user found"
+      return render :plain => "No user found"
+    end
+
+    if user.account_activation_token == nil
+      return redirect_to "#{Rails.application.credentials.web_app_url}/login?already_activated=true"
+    end
+
+    if user.account_activation_token != params[:token]
+      return render :plain => "Invalid account activation token"
     end
 
     user.update(account_activation_token: nil)
