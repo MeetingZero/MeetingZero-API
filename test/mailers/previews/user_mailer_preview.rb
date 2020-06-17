e20b6e9a131b58a06c5770633ac7e3bc1821612f
Arun Sood <arsood@gmail.com>
2020-06-17 02:09:30 -0700

    Activate account email styling

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,5 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def activate_account
+    UserMailer.activate_account(User.first)
+  end
+
   def forgot_password
     UserMailer.forgot_password(User.first)
   end
