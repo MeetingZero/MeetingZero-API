dfbb777ffa1a3cac1c3f90f94c9aa1b435f85d89
Arun Sood <arsood@gmail.com>
2020-06-17 02:05:06 -0700

    Styling for forgot password email

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+  def forgot_password
+    UserMailer.forgot_password(User.first)
+  end
 end
