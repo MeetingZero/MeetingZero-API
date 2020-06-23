6d2d62d6b5014b9dc73ce252bb95a4024473938e
Arun Sood <arsood@gmail.com>
2020-06-22 21:31:48 -0700

    Includes for directors

--- app/controllers/api/v1/workshop_directors_controller.rb
+++ app/controllers/api/v1/workshop_directors_controller.rb
@@ -3,22 +3,24 @@ class Api::V1::WorkshopDirectorsController < ApplicationController
 
   def index
     workshop_directors = Workshop
-    .find(params[:workshop_id])
+    .where(workshop_token: params[:workshop_id])
+    .first
     .workshop_directors
     .all
     .order(id: :asc)
 
-    return render :json => workshop_directors
+    return render :json => workshop_directors.group_by { |i| i.workshop_stage_id }, include: [:workshop_stage, :workshop_stage_step]
   end
 
   def current
     workshop_director = Workshop
-    .find(params[:workshop_id])
+    .where(workshop_token: params[:workshop_id])
+    .first
     .workshop_directors
     .where(completed: false)
     .order(id: :asc)
     .first
 
-    return render :json => workshop_director
+    return render :json => workshop_director, include: [:workshop_stage, :workshop_stage_step]
   end
 end
--- app/models/workshop_director.rb
+++ app/models/workshop_director.rb
@@ -1,3 +1,5 @@
 class WorkshopDirector < ApplicationRecord
   belongs_to :workshop
+  belongs_to :workshop_stage
+  belongs_to :workshop_stage_step
 end
