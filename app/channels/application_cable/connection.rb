0be20b39dced8511a45966b769214efc6bdb70bc
Arun Sood <arsood@gmail.com>
2020-06-26 18:48:41 -0700

    Set up cable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,34 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      auth_token = params[:token]
+    
+      if !auth_token
+        return reject_unauthorized_connection
+      end
+
+      decoded_token = JWT.decode(
+        auth_token,
+        Rails.application.credentials.jwt_secret,
+        true,
+        { algorithm: 'HS256' }
+      )
+
+      if !decoded_token
+        return reject_unauthorized_connection
+      end
+
+      current_user = User.find(decoded_token[0]["id"])
+
+      return current_user
+    end
   end
 end
