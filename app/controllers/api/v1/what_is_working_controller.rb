4d2405528f43227dd9524ff59dfd588d787130ce
Arun Sood <arsood@gmail.com>
2020-07-08 01:10:48 -0700

    Save what is working response to DB

--- app/controllers/api/v1/what_is_working_controller.rb
+++ app/controllers/api/v1/what_is_working_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::WhatIsWorkingController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user
-  before_action :authorize_user_for_workshop, only: [:create]
+  before_action :authorize_user_for_workshop, only: [:index, :create]
+
+  def index
+    what_is_working_records = WhatIsWorkingResponse
+    .where(
+      workshop_id: @workshop.id,
+      user_id: @current_user.id
+    )
+
+    return render :json => what_is_working_records
+  end
 
   def create
     what_is_working_response = WhatIsWorkingResponse
@@ -11,6 +21,12 @@ class Api::V1::WhatIsWorkingController < ApplicationController
       response_text: params[:response_text]
     )
 
-    return render :json => what_is_working_response, status: 201
+    what_is_working_records = WhatIsWorkingResponse
+    .where(
+      workshop_id: @workshop.id,
+      user_id: @current_user.id
+    )
+
+    return render :json => what_is_working_records, status: 201
   end
 end
