19695e8a22677a37e97dafad145b68f590640cd5
Arun Sood <arsood@gmail.com>
2020-08-11 22:18:50 -0700

    Add algorithm for assessment category

--- app/models/solution_response_priority.rb
+++ app/models/solution_response_priority.rb
@@ -1,2 +1,18 @@
 class SolutionResponsePriority < ApplicationRecord
+  before_create :assign_assessment_category
+
+  def assign_assessment_category
+    impact_level = self.impact_level
+    effort_level = self.effort_level
+
+    if impact_level > 5 && effort_level > 5
+      self.assessment_category = "Make a Project"
+    elsif impact_level > 5 && effort_level <= 5
+      self.assessment_category = "Do Now"
+    elsif impact_level <= 5 && effort_level > 5
+      self.assessment_category = "Forget for Now"
+    elsif impact_level <= 5 && effort_level <= 5
+      self.assessment_category = "Make a Task"
+    end
+  end
 end
