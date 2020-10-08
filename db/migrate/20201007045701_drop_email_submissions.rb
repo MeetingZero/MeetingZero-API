daa6e1f2b222b0bcd2816801956e0a6e5ca25e60
Arun Sood <arsood@gmail.com>
2020-10-08 10:35:41 -0700

    Remove drop email submissions

--- db/migrate/20201007045701_drop_email_submissions.rb
+++
@@ -1,9 +0,0 @@
-class DropEmailSubmissions < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :email_submissions
-  end
-
-  def down
-    fail ActiveRecord::IrreversibleMigration
-  end
-end
