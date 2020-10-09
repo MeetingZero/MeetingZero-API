295d542949586a7836d20b1d86a43f134a713b90
Arun Sood <arsood@gmail.com>
2020-10-08 23:54:15 -0700

    Environment Sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production" || Rails.env == "staging"
   Raven.configure do |config|
-    config.dsn = "https://93b687fb7ead48d1ad6a5402823bc71b@o459140.ingest.sentry.io/5457845"
+    config.dsn = Rails.application.credentials.sentry_dsn
   end
 end
