61ab12c9a3d57040c8a819af4a95ac6e81f8a42e
Arun Sood <arsood@gmail.com>
2020-10-08 22:17:56 -0700

    Use Sentry in separate project

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production" || Rails.env == "staging"
   Raven.configure do |config|
-    config.dsn = "https://00b9c02e492644b7b3ceb423c2e38b51@o459140.ingest.sentry.io/5457814"
+    config.dsn = "https://93b687fb7ead48d1ad6a5402823bc71b@o459140.ingest.sentry.io/5457845"
   end
 end
