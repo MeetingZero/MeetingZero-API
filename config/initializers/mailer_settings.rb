4dc6628345d03429477934801f111ccb989c7953
Arun Sood <arsood@gmail.com>
2021-01-20 09:37:03 -0800

    SMTP settings for Sendgrid

--- config/initializers/mailer_settings.rb
+++ config/initializers/mailer_settings.rb
@@ -4,10 +4,10 @@ if ENV["RAILS_ENV"] == "staging" || ENV["RAILS_ENV"] == "production"
   ActionMailer::Base.smtp_settings = {
     address: "smtp.sendgrid.net",
     domain: "meetingzero.net",
-    port: 465,
+    port: 587,
     user_name: "apikey",
     password: Rails.application.credentials.sendgrid_key,
-    authentication: :ssl,
+    authentication: :plain,
     enable_starttls_auto: true
   }
 end
